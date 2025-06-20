b14fe6d14b317a39c8169b01f77d2f2b30849996
kailasnv <itskailasnv@gmail.com>
2025-06-20 17:30:22 +0000

    ux

--- yt-terminal.py
+++ yt-terminal.py
@@ -2,6 +2,7 @@
 import subprocess
 import sys
 import shutil
+import threading
 
 
 
@@ -30,8 +31,9 @@ def search_for_songs(query, terminal_width):
         stderr=subprocess.PIPE #error
         )
         if (result.returncode == 0):
-            text = "search success"
+            text = "- search success -"
             print(f"{Colors.GREEN}{Styles.italic}{text.center(terminal_width)}{Styles.italic}{Colors.GREEN}")
+            print(f"{Colors.CYAN}{Styles.italic}{"press enter to stop song/search new song".center(terminal_width)}{Styles.italic}{Colors.CYAN}")
         else:
             print(f"{Colors.RED}nothing found! search again.{Colors.RED}")
              
@@ -46,13 +48,33 @@ def search_for_songs(query, terminal_width):
         print(f"{Colors.RED}{result.stderr}{Colors.RED}")
  
 
+#clean a line
+def moveup_and_clean_line(moveup, terminal_width):
+    for i in range(moveup):  
+        sys.stdout.write('\r' + ' ' * terminal_width + '\r')
+        sys.stdout.write('\033[F')
+        sys.stdout.write('\r' + ' ' * terminal_width + '\r')
 
+    # end if
 
 # play song using mpv
-def play_song(url):
+def play_song(url, terminal_width):
     try:
         # play song using mpv
-        subprocess.run(["mpv", "--no-video", url])
+        process = subprocess.Popen(["mpv", "--no-video", url])
+
+        def listen_for_enterkey():
+            while (True):
+                key = input()
+                if key == '':
+                    process.terminate()
+                    break
+                    break                
+                # end if 
+            # end while
+        input_thread = threading.Thread(target=listen_for_enterkey(), daemon=True)
+        input_thread.start()
+        process.wait()
     except FileNotFoundError as e:
         print(f"{e}")
         print("please install mpv in your system.")
@@ -82,6 +104,7 @@ def main():
         #user input:
         query = input(f"{Colors.CYAN}search any song: ")
         if (query == ""):
+            sys.stdout.write('\033[F') # ensures always on same line
             continue
         # if there is a input
         sys.stdout.write("\033[F")  #move cursor up one line
@@ -90,7 +113,7 @@ def main():
         
         
         url = search_for_songs(query, terminal_width)
-        play_song(url)
+        play_song(url, terminal_width)
     # end while
 
 
